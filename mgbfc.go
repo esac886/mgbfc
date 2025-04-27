b8020cd683635bd8723a7ae21eea7f1ce74508a2
Kirill Rusnik <rusni1kk@gmail.com>
2025-04-28 02:09:50 +0700

    Renamed constants, substituted newline and concat with multiline strings

--- mgbfc.go
+++ mgbfc.go
@@ -10,47 +10,58 @@ import (
 	"os/exec"
 )
 
-// TODO const naming
 const (
-	Rodata = ".section .rodata\n" +
-		".set tape_size, %d\n\n"
-	Bss = ".section .bss\n" +
-		".lcomm tape, tape_size\n" +
-		".lcomm in_buf, 1\n\n" // TODO maybe alloc input buf if necessary
-	Text = ".section .text\n" +
-		".global _start\n\n" +
-		"_start:\n" +
-		"    xor     %r9,            %r9\n" +
-		"    xor     %r10,           %r10\n" +
-		"    lea     tape(%rip),     %r10\n" +
-		"    xor     %rsi,           %rsi\n"
-	Epilogue = "    mov     $60,            %rax\n" +
-		"    xor     %rbx,           %rbx\n" +
-		"    syscall\n"
-	Input = "    mov     $0,              %rax\n" +
-		"    mov     $0,              %rdi\n" +
-		"    mov     $1,              %rdx\n" +
-		"    lea     in_buf(%rip),    %rsi\n" +
-		"    syscall\n" +
-		"    movb    (%rsi),          %r11b\n" +
-		"    movb    %r11b,           (%r10, %r9, 1)\n"
-	Output = "    mov     $1,             %rax\n" +
-		"    mov     $1,             %rdi\n" +
-		"    lea     (%r10, %r9, 1), %rsi\n" +
-		"    mov     $1,             %rdx\n" +
-		"    syscall\n"
-	CycleStart = "\ns%c:\n" +
-		"    cmpb    $0,             (%%r10, %%r9, 1)\n" +
-		"    je      e%c\n"
-	CycleEnd = "    cmpb    $0,             (%%r10, %%r9, 1)\n" +
-		"    jg      s%c\n\n" +
-		"e%c:\n"
-	Increment       = "    addb    $1,             (%r10, %r9, 1)\n"
-	Decrement       = "    subb    $1,             (%r10, %r9, 1)\n"
-	IncrDataPtr     = "    add     $1,             %r9w\n"
-	DecrDataPtr     = "    sub     $1,             %r9w\n"
-	ResetDataPtr    = "    mov     $0,             %r9w\n"
-	MaximizeDataPtr = "    mov     $tape_size,     %r9w\n"
+	rodata = `.section .rodata
+.set tape_size, %d`
+
+	bss = `.section .bss
+.lcomm tape, tape_size
+.lcomm in_buf, 1
+` // TODO maybe alloc input buf if necessary
+
+	text = `.section .text
+.global _start
+
+_start:
+    xor     %r9,            %r9
+    xor     %r10,           %r10
+    lea     tape(%rip),     %r10
+    xor     %rsi,           %rsi`
+
+	epilogue = `    mov     $60,            %rax
+    xor     %rbx,           %rbx
+    syscall`
+
+	input = `    mov     $0,              %rax
+    mov     $0,              %rdi
+    mov     $1,              %rdx
+    lea     in_buf(%rip),    %rsi
+    syscall
+    movb    (%rsi),          %r11b
+    movb    %r11b,           (%r10, %r9, 1)`
+
+	output = `    mov     $1,             %rax
+    mov     $1,             %rdi
+    lea     (%r10, %r9, 1), %rsi
+    mov     $1,             %rdx
+    syscall`
+
+	cycleStart = `
+%c:
+    cmpb    $0,             (%%r10, %%r9, 1)
+    je      e%c`
+
+	cycleEnd = `    cmpb    $0,             (%%r10, %%r9, 1)
+    jg      s%c
+
+e%c:`
+
+	increment       = "    addb    $1,             (%r10, %r9, 1)\n"
+	decrement       = "    subb    $1,             (%r10, %r9, 1)\n"
+	incrDataPtr     = "    add     $1,             %r9w\n"
+	decrDataPtr     = "    sub     $1,             %r9w\n"
+	resetDataPtr    = "    mov     $0,             %r9w\n"
+	maximizeDataPtr = "    mov     $tape_size,     %r9w\n"
 )
 
 var logger = log.New(os.Stderr, "", 0)
@@ -120,9 +131,9 @@ func main() {
 
 		// generating prologue
 		// TODO struct method to get rid of name and path in every func call
-		write(fmt.Sprintf(Rodata, tapeSize), tmpAsm)
-		write(Bss, tmpAsm)
-		write(Text, tmpAsm)
+		write(fmt.Sprintf(rodata, tapeSize), tmpAsm)
+		write(bss, tmpAsm)
+		write(text, tmpAsm)
 
 		// TODO expand labels range. now it is between 65 (A) to 90 (Z)
 		curLabel := -1
@@ -140,7 +151,7 @@ func main() {
 			if err != nil {
 				if err == io.EOF {
 					// TODO verbose output
-					write(Epilogue, tmpAsm)
+					write(epilogue, tmpAsm)
 					break
 				} else {
 					logger.Fatalf("%s:%d:%d ERROR: %s\n", srcPath, line, col, err.Error())
@@ -182,17 +193,17 @@ func main() {
 					write("    sub     $1,             %r9w\n", tmpAsm)
 				}
 			case ',':
-				write(Input, tmpAsm)
+				write(input, tmpAsm)
 			case '.':
-				write(Output, tmpAsm)
+				write(output, tmpAsm)
 			case '[':
 				// TODO brackets check
 				labelChar++
 				curLabel++
 				labels[curLabel] = labelChar
-				write(fmt.Sprintf(CycleStart, labelChar, labelChar), tmpAsm)
+				write(fmt.Sprintf(cycleStart, labelChar, labelChar), tmpAsm)
 			case ']':
-				write(fmt.Sprintf(CycleEnd, labels[curLabel], labels[curLabel]), tmpAsm)
+				write(fmt.Sprintf(cycleEnd, labels[curLabel], labels[curLabel]), tmpAsm)
 				curLabel--
 			case '#':
 				// TODO better skipping
